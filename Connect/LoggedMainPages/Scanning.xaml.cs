c8ff7f7fd8098386655a2d45facaece8fd7089cf
guidufort <guidufort@msn.com>
2013-09-25 20:55:03 -0300

    Caso de uso Login, y Singleton que guarda usuario logueado. Se debe emprolijar codigo.

--- Connect/LoggedMainPages/Scanning.xaml.cs
+++ Connect/LoggedMainPages/Scanning.xaml.cs
@@ -44,26 +44,6 @@ namespace Connect.LoggedMainPages
         }
 
 
-        void SplashPage_Loaded(object sender, RoutedEventArgs e)
-        {
-            request = (HttpWebRequest)HttpWebRequest.Create(new Uri("http://www.developer.nokia.com/Community/Wiki/Portal:Windows_Phone_UI_Articles"));
-            request.BeginGetResponse(new AsyncCallback(ReceiveResponseCallBack), null);
-        }
-
-        private void ReceiveResponseCallBack(IAsyncResult result)
-        {
-            HttpWebResponse response = (HttpWebResponse)this.request.EndGetResponse(result);
-            using (var stream = response.GetResponseStream())
-            {
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    string str = reader.ReadToEnd();
-                    Deployment.Current.Dispatcher.BeginInvoke(() =>
-                    {
-                        NavigationService.Navigate(new Uri("/LoggedMainPages/Done.xaml", UriKind.Relative));
-                    });
-                }
-            }
-        }
+        
     }
 }
--- Connect/MainPage.xaml.cs
+++ Connect/MainPage.xaml.cs
@@ -8,19 +8,44 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using Connect.Resources;
+using System.IO;
+using System.Windows.Threading;
+using Newtonsoft.Json;
+using Connect.Classes;
+
+
+
 
 namespace Connect
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        static private Uri usuario = new Uri("http://connectwp.azurewebsites.net/api/Users/10");       
+        private UserData UsuarioLogin;
         // Constructor
         public MainPage()
         {
             InitializeComponent();
-            Loaded += MainPage_Loaded;
+
+            Loaded += MainPage_Loaded;         
+            WebClient webClient = new WebClient();
+            webClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(webClient_DownloadStringCompleted);
+            webClient.DownloadStringAsync(usuario); 
+            
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
         }
+
+
+
+        void webClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            UsuarioLogin = JsonConvert.DeserializeObject<UserData>(e.Result);        
+            MailIngresado.Text = UsuarioLogin.Email;
+            PassIngresado.Password = UsuarioLogin.Password;
+        }
+
+       
 
         void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
@@ -31,21 +56,73 @@ namespace Connect
         {
             NavigationService.Navigate(new Uri("/LoggedMainPages/Register.xaml", UriKind.Relative));
         }
-
-
-        private void Click_check(object sender, EventArgs e)
+        
+        private void sendPostCompleted(object sender, UploadStringCompletedEventArgs e)
         {
-            if (((MailIngresado.Text == "guille") && (PassIngresado.Password == "pass")) || ((MailIngresado.Text == "") && (PassIngresado.Password == "")))
+            if ((e.Error!= null) && (e.Error.GetType().Name == "WebException"))
             {
-                ErrorBlock.Visibility = System.Windows.Visibility.Collapsed;
-                NavigationService.Navigate(new Uri("/LoggedMainPages/LoggedMainPage.xaml", UriKind.Relative));
+                WebException we = (WebException)e.Error;
+                HttpWebResponse response = (System.Net.HttpWebResponse)we.Response;
+                  
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.NotFound: // 404
+                        System.Diagnostics.Debug.WriteLine("Not found!");
+                        ErrorBlock.Text = "That email is not registered";
+                        ErrorBlock.Visibility = System.Windows.Visibility.Visible;
+                        break;
+                    case HttpStatusCode.Unauthorized: // 401
+                        System.Diagnostics.Debug.WriteLine("Not authorized!");                        
+                        ErrorBlock.Text = "The password is not correct";
+                        ErrorBlock.Visibility = System.Windows.Visibility.Visible;
+                        break;
+                    default:
+                        break;
+                }
             }
             else
             {
-                ErrorBlock.Visibility = System.Windows.Visibility.Visible;
+                UsuarioLogin = JsonConvert.DeserializeObject<UserData>(e.Result);
+                ErrorBlock.Visibility = System.Windows.Visibility.Collapsed;
+                NavigationService.Navigate(new Uri("/LoggedMainPages/LoggedMainPage.xaml", UriKind.Relative));
+            }
+        }
+
+
+        private void Click_check(object sender, EventArgs e)        
+        {
+            try
+            {
+                var webClient = new WebClient();
+                webClient.Headers[HttpRequestHeader.ContentType] = "text/json";
+                webClient.UploadStringCompleted += this.sendPostCompleted;
+
+                string json = "{\"Email\":\"" + MailIngresado.Text +  "\"," +
+                                  "\"Password\":\"" + PassIngresado.Password + "\"}";
+
+                webClient.UploadStringAsync((new Uri("http://connectwp.azurewebsites.net/api/login/")), "POST", json);
+            }
+            catch (WebException webex)
+            {
+                HttpWebResponse webResp = (HttpWebResponse)webex.Response;
+
+                switch (webResp.StatusCode)
+                {
+                    case HttpStatusCode.NotFound: // 404
+                        break;
+                    case HttpStatusCode.InternalServerError: // 500
+                        break;
+                    default:
+                        break;
+                }
             }
 
+
         }
+
+   
+
+        
 
       
 
