1c79b56a77c33c4877f096734a9e0fbb74c9dedb
guidufort <guidufort@msn.com>
2013-10-30 15:00:32 -0200

    Cambio del link a donde enviar en la pantalla de amigos

--- Connect/LoggedMainPages/LinkFriend.xaml.cs
+++ Connect/LoggedMainPages/LinkFriend.xaml.cs
@@ -19,10 +19,7 @@ namespace Connect.LoggedMainPages
         {
             InitializeComponent();
             LoggedUser lu = LoggedUser.Instance;
-            this.loginBrowserControl.Navigate(new Uri("http://www.linkedin.com/profile/view?id="+lu.friendInf.LinkedInId, UriKind.Absolute));//aca va el linkedin del nuevo amigo
-
-        }
-
-       
+            this.loginBrowserControl.Navigate(new Uri("http://www.linkedin.com/profile/view?id=" + lu.friendInf.LinkedInId + "&authType=NAME_SEARCH&authToken=POOH&locale=en_US&srchid=2875693011383147764200&srchindex=1&srchtotal=1&trk=vsrp_people_res_name&trkInfo=VSRPsearchId%3A2875693011383147764200%2CVSRPtargetId%3A709226%2CVSRPcmpt%3Aprimary", UriKind.Absolute));//aca va el linkedin del nuevo amigo
+        }       
     }
 }
